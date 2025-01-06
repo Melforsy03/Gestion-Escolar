90be47a6230b42acf2bc84f98f44109283c32917
username <ibaezmustelier@gmail.com>
2025-01-06 10:18:20 -0500

    Role

--- ASP.Net-backend/SchoolManagement.Domain/obj/Debug/net8.0/SchoolManagement.Domain.AssemblyInfo.cs
+++ ASP.Net-backend/SchoolManagement.Domain/obj/Debug/net8.0/SchoolManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6961df21b6ed3cd46a34aedf3b4368070693726a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b6c49e6b5c61add5d295d6549ce8a83add07eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
