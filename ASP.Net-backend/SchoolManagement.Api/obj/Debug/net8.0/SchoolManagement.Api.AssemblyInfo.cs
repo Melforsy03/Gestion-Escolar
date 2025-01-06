cfccf06f28551fcb82eda8c6fd5bd08b49b254aa
username <ibaezmustelier@gmail.com>
2025-01-06 11:21:37 -0500

    1

--- ASP.Net-backend/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
+++ ASP.Net-backend/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6961df21b6ed3cd46a34aedf3b4368070693726a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff636449d477b6bc5e8483b3db1b2ed0ab49b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
