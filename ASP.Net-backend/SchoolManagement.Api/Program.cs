970cb201399539d0e940b565acface6de8162f52
username <ibaezmustelier@gmail.com>
2025-01-20 08:56:47 -0500

    Actualizazcion Autenticacion

--- ASP.Net-backend/SchoolManagement.Api/Program.cs
+++ ASP.Net-backend/SchoolManagement.Api/Program.cs
@@ -1,26 +1,31 @@
 using Microsoft.AspNetCore.DataProtection.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SchoolManagement.Application;
 using SchoolManagement.Application.ApplicationServices.IServices;
 using SchoolManagement.Application.ApplicationServices.Maps_Dto;
 using SchoolManagement.Application.ApplicationServices.Services;
+using SchoolManagement.Application.Authentication;
+using SchoolManagement.Application.Interfaces;
 using SchoolManagement.Infrastructure;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
 
-
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 builder.Services.AddControllers();
 
 var services = builder.Services;
 
 services.AddHttpContextAccessor();
-services.AddApplicationServices();
+services.AddApplicationServices(builder.Configuration);
 services.AddInfraestructureServices();
 
-builder.Services.AddInfraestructureServices();
+
+//builder.Services.AddInfraestructureServices();
 
 builder.Services.AddDbContext<Context>(options =>
 {
@@ -28,7 +33,26 @@ builder.Services.AddDbContext<Context>(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
         
 var app = builder.Build();
 
@@ -38,8 +62,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
