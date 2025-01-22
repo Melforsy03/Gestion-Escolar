acb3938a60ea705f8f46dffdc01069b27baa6cd4
Melforsy03 <forsythemelani51@gmail.com>
2025-01-22 08:46:09 -0500

    .

--- ASP.Net-backend/SchoolManagement.Api/Controllers/IdentityController.cs
+++ ASP.Net-backend/SchoolManagement.Api/Controllers/IdentityController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagement.Application.ApplicationServices.IServices;
 using SchoolManagement.Application.ApplicationServices.Maps_Dto;
