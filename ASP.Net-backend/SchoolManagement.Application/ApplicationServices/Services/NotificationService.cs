280e56195293cdc94b0534d15e4af425c1cd3d8a
rizhex <rizhedev@gmail.com>
2025-02-02 18:46:43 -0500

    alertsEndpoint

---
+++ ASP.Net-backend/SchoolManagement.Application/ApplicationServices/Services/NotificationService.cs
@@ -0,0 +1,18 @@
+﻿using SchoolManagement.Application.ApplicationServices.IServices;
+using SchoolManagement.Application.ApplicationServices.Maps_Dto.Notification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.ApplicationServices.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public async Task<NotificationResponse> GetNotification(string UserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
